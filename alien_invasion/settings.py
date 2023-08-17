48d03275d31d1493d89cbce4c715ddc14d0593c0
Abukhadeejah <arsalanahmed82@hotmail.com>
2023-08-17 10:43:22 +0530

    added setting for ship speed control

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -6,3 +6,6 @@ class Settings():
         self.screen_width = 1400
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
+
+        # Ship speed settings
+        self.ship_speed_factor = 1.5
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -3,9 +3,10 @@ import pygame
 class Ship():
     """ Initialize the ship object on screen. """
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         # Create screen object and place the ship at the bottom of the screen
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the image and get its rectangle
         self.image = pygame.image.load('images/ship.bmp')
@@ -20,14 +21,19 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # store dcimal value for the ships center
+        self.center = float(self.rect.centerx)
+
 
     def update(self):
         # Update the ships position  based on movement flag
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
 
+        # update the rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         # Draw the ship at its current position
